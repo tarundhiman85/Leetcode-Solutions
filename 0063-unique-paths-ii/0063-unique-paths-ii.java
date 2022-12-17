a50d5c41ea722c10b872ea5edbf2248c8e6c961f
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2022-12-17 20:31:07 +0530

    Time: 1 ms (77.04%), Space: 42.9 MB (9.27%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,7 +3,11 @@ class Solution {
         int n = grid[0].length;
         int m = grid.length;
         int[][] dp = new int[m+1][n+1];
-
+        for(int i=0; i<=m; i++){
+            for(int j=0; j<=n; j++){
+                dp[i][j]=0;
+            }
+        }
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)

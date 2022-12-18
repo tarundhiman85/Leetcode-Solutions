992f6f702a5d3e6388d533168b8ba2fdedee667c
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2022-12-18 18:01:37 +0530

    Time: 29 ms (6.06%), Space: 44.8 MB (11.74%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -18,6 +18,33 @@ class Solution {
     public int minimumTotal(List<List<Integer>> arr) {
         int n = arr.size();
         Integer[][] dp = new Integer[n][n];
-        return solve(arr, n, 0, 0, dp);
+        //return solve(arr, n, 0, 0, dp);
+        //Tabulation
+        for(int i=0;i<n;i++)
+        {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+      int minV = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<arr.get(i).size(); j++)
+            {
+
+                if(i==0 && j==0)
+                {
+                    dp[i][j] = arr.get(i).get(j);
+                    if(i==n-1) minV = Math.min(minV, dp[i][j]);
+                }
+                else{
+                    int dr = Integer.MAX_VALUE;
+                    int d = Integer.MAX_VALUE;
+                    if(i>0 && j>0) dr = dp[i-1][j-1];
+                    if(i>0) d = dp[i-1][j];
+                    dp[i][j] = arr.get(i).get(j) + Math.min(dr, d);
+                    if(i==n-1) minV = Math.min(minV, dp[i][j]);
+                }
+            }
+        }
+        return minV;
     }
 }

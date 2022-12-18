53a27f5ddf7a340885f1a9cb20c73628b53dcec9
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2022-12-18 10:57:33 +0530

    Time: 271 ms (5.15%), Space: 136 MB (41.74%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,18 +1,22 @@
 class Solution {
     public int[] dailyTemperatures(int[] arr) {
         Stack<Integer> st = new Stack<>();
-        int n = arr.length;
-        int[] output = new int[n];
-        if(n==0) return output;
-        for(int i=0; i<n; i++)
-        {
-            while(!st.isEmpty() && arr[st.peek()]<arr[i])
-            {
-                int popped = st.pop();
-                output[popped] = i - popped;
-            }
-            st.push(i);
-        }
-        return output;
+        int[] output = new int[arr.length];
+        if(arr.length==0) return output;
+
+       for(int i=arr.length-1; i>=0; i--){
+           while(!st.isEmpty() && arr[i]>=arr[st.peek()]){
+               st.pop();  //we will get the last element
+               //we will place
+           }
+           if(!st.isEmpty() && arr[i]<arr[st.peek()]){
+               //we will check if the element is hotter then put that element to the result
+               //by calculating the distance
+               int distance=st.peek()-i;
+               output[i]=distance;
+           }
+           st.push(i);
+       }
+       return output;
     }
 }

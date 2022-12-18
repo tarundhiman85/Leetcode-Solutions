ae5d1099f18808545af37c40832966f41ed3289b
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2022-12-18 16:42:00 +0530

    Time: 5 ms (48.34%), Space: 46.5 MB (11.34%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -14,32 +14,24 @@ class Solution {
      
     }
     public int uniquePaths(int m, int n, int [][] grid) {
-        int[][] dp = new int[m+1][n+1];
-        for(int i=0; i<m; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                dp[i][j]=-1;
-            }
-        }
-        //Tabulation
-        // for(int i=0; i<m; i++)
-        // {
-        //     for(int j=0; j<n; j++)
-        //     {
-        //         if(i==0 && j==0){ 
-        //             dp[i][j]=1;
-        //             continue;
-        //         }
-        //         int down=0;
-        //         int right=0;
-        //         if(i>0) down = dp[i-1][j];
-        //         if(j>0) right = dp[i][j-1];
-        //         dp[i][j] = down+right;
-        //     }
-        // }
-        return f(0,0,m,n, grid, dp);
-        // return dp[m-1][n-1];
+        int[][] dp = new int[m][n];
+         //Tabulation
+         for(int i=0; i<m; i++)
+         {
+             for(int j=0; j<n; j++)
+             {
+                 if(i==0 && j==0){
+                     dp[i][j]=grid[i][j];
+                     continue;
+                 }
+                 int up = Integer.MAX_VALUE;
+                 int down = Integer.MAX_VALUE;
+                 if(i>0) up = dp[i-1][j];
+                 if(j>0) down = dp[i][j-1];
+                 dp[i][j] = grid[i][j] + Math.min(up, down);
+             }
+         }
+        return dp[m-1][n-1];
     }
     public int minPathSum(int[][] grid) 
     {

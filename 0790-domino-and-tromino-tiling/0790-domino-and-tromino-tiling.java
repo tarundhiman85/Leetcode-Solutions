2d4c89fe57ef4d41b835f0a9f0bfcec73d3ba872
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2022-12-24 14:34:45 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (96.38%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -1,21 +1,40 @@
 class Solution {
-       
-    int MOD = 1_000_000_007;
-    int[] dp;
+     //Memoization  
+//     int MOD = 1_000_000_007;
+//     int[] dp;
     
+//     public int numTilings(int n) {
+//         dp = new int[n+1];
+//         Arrays.fill(dp, -1);
+//         return helper(n);
+//     }
+    
+//     int helper(int n) {
+//         if(n < 0) return 0;
+//         if(n == 0) return 1;
+//         if(n <= 2) return n;
+        
+//         if(dp[n] != -1) return dp[n];
+        
+//         return dp[n] = ((2 * helper(n-1)) % MOD + (1 * helper(n-3)) % MOD) % MOD;
+//     }
+    //Tabulation
+      private final static int mod = 1000000007;
     public int numTilings(int n) {
-        dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return helper(n);
-    }
-    
-    int helper(int n) {
-        if(n < 0) return 0;
-        if(n == 0) return 1;
-        if(n <= 2) return n;
         
-        if(dp[n] != -1) return dp[n];
+        if(n == 1) return  1;
+        if(n == 2) return 2;
         
-        return dp[n] = ((2 * helper(n-1)) % MOD + (1 * helper(n-3)) % MOD) % MOD;
+        int dp[] = new int[n+1];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++){
+            dp[i] = ((2*dp[i-1])%mod + dp[i-3])%mod;
+        }
+        
+        return dp[n];
     }
 }

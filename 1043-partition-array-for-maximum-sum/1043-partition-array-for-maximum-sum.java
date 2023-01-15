9cd9c91a2039757a7c8adc8f869a0dd532e3cd03
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2023-01-15 19:06:26 +0530

    Time: 6 ms (85.69%), Space: 41.7 MB (77.60%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    int f(int[] arr, int i, Integer[] dp, int k)
+    {
+        int n = arr.length;
+        if(i>=n) return 0;
+        if(dp[i]!=null) return dp[i];
+        int sum = 0;
+        int maxi = Integer.MIN_VALUE;
+        int maxAns = Integer.MIN_VALUE;
+        int len = 0;
+        for(int j=i; j<Math.min(i+k, n); j++)
+        {
+            len++;
+            maxi = Math.max(arr[j], maxi);
+            sum = (len * maxi) + f(arr, j+1, dp, k);
+            maxAns = Math.max(maxAns, sum);
+        }
+        return dp[i] = maxAns;
+    }
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        Integer[] dp = new Integer[n+2];
+        return f(arr, 0, dp, k);
+    }
+}

198e020635999074560c92472a1c255c304721fc
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2023-01-15 18:29:33 +0530

    Time: 1353 ms (26.26%), Space: 39.8 MB (99.07%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -29,7 +29,22 @@ class Solution {
     }
     public int minCut(String s) {
         int n = s.length();
-        Integer[] dp = new Integer[n+1];
-        return f(0,n,s,dp)-1;
+        // Integer[] dp = new Integer[n+1];
+        // return f(0,n,s,dp)-1;
+        int[] dp = new int[n+1];
+        for(int i=n-1; i>=0; i--)
+        {
+                int mini = Integer.MAX_VALUE;
+                for(int j=i; j<n; j++)
+                {
+                    if(isPalindrome(i, j, s))
+                    {
+                        int cost = 1+dp[j+1];
+                        mini = Math.min(mini, cost);
+                    }
+                }
+                dp[i] = mini;
+        }
+        return dp[0]-1;
     }
 }

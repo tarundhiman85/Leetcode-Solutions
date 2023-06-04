64bb697284d4118a78f58bc0d6f03961b8082edf
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2023-06-04 11:13:04 +0530

    Time: 2 ms (97.34%), Space: 46.5 MB (47.19%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+       // Step 1: Initialize candidates and counters
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+              // Step 2: Find potential candidates
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+                // Step 3: Validate the candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+
+        // Step 4: Add the valid candidates to the result
+        if (count1 > nums.length / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > nums.length / 3) {
+            result.add(candidate2);
+        }
+
+        return result;
+    }
+}

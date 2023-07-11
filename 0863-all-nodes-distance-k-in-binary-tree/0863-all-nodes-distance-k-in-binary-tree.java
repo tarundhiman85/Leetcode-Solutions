533a1812134d49ea643443f601dfca221049aace
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2023-07-11 18:32:15 +0530

    Time: 13 ms (45.52%), Space: 42.2 MB (33.75%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<TreeNode, TreeNode> parent_track = new HashMap<>();
+        markParents(root, parent_track, root);
+        Map<TreeNode, Boolean> visited = new HashMap<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(target);
+        visited.put(target, true);
+        int curr_level = 0;
+        while(!queue.isEmpty())
+        {
+            int size = queue.size();
+            if(curr_level == k) break;
+            curr_level++;
+            while(size-->0)
+            {
+                TreeNode current = queue.poll();
+                if(current.left!=null && visited.get(current.left)==null)
+                {
+                    queue.offer(current.left);
+                    visited.put(current.left, true);
+                }
+                if(current.right!=null && visited.get(current.right)==null)
+                {
+                    queue.offer(current.right);
+                    visited.put(current.right, true);
+                }
+                if(parent_track.get(current)!=null && visited.get(parent_track.get(current))==null)
+                {
+                    queue.offer(parent_track.get(current));
+                    visited.put(parent_track.get(current), true);
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        while(!queue.isEmpty())
+        {
+            TreeNode current = queue.poll();
+            result.add(current.val);
+        }
+        return result;
+    }
+    public void markParents(TreeNode root, Map<TreeNode, TreeNode> parent_track, TreeNode target)
+    {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty())
+        {
+            TreeNode current = queue.poll();
+            if(current.left != null)
+            {
+                parent_track.put(current.left, current);
+                queue.offer(current.left);
+            }
+            if(current.right != null)
+            {
+                parent_track.put(current.right, current);
+                queue.offer(current.right);
+            }
+        }
+    }
+}

e39c6a336679d594d9a7cdfaf61595701c75be58
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2023-08-08 15:55:26 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (85.30%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -8,22 +8,27 @@ class Solution {
         {
             int mid = low +(high-low)/2;
             if(arr[mid]==target) return mid;
-            //we need to first see the proper alignment
+            //the left side is sorted
             if(arr[mid]>=arr[low])
             {
+                //figure out if element lies on the left half  eliminate right half
                 if(arr[low]<=target && arr[mid]>=target)
                 {
                     high=mid-1;
                 }
+                //eliminate the left half
                 else{
                     low=mid+1;
                 }
             }
+            //right half is sorted
             else{
+                //figure out if element lies on right half eliminate left half
                 if(arr[mid]<=target && arr[high]>=target)
                 {
                     low=mid+1;
                 }
+                //eliminate the right half
                 else{
                     high=mid-1;
                 }

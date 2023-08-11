69dc0e7f2ab8f0310cff9922d5cd12c859999162
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2023-08-11 23:01:49 +0530

    Time: 4342 ms (13.76%), Space: 16.3 MB (71.79%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,21 +1,15 @@
 class Solution:
     st = ""
     cnt = 0
-
     def f(self, c, n, vow, k):
         if c == 0:
             self.cnt += 1
-            self.st = self.st[:-1]
             return
         if k >= len(vow):
             return
-
-        self.st = self.st + vow[k]
         self.f(c - 1, n, vow, k)
         self.f(c, n, vow, k + 1)
-        if len(self.st) > 0:
-            self.st = self.st[:-1]
-
     def countVowelStrings(self, n: int) -> int:
+        
         self.f(n, n, ['a', 'e', 'i', 'o', 'u'], 0)
         return self.cnt

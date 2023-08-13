3f6606fd90b771572c443ea2f10eb8ba71f3a614
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2023-08-13 22:46:43 +0530

    Time: 13 ms (18.81%), Space: 45.2 MB (25.74%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -14,12 +14,30 @@ class Solution {
     public boolean stoneGame(int[] piles) {
         int n = piles.length;
         int[][] dp = new int[n][n];
-        int res = f(piles, 0, n-1, dp);
-        if(res >0)
+        // int res = f(piles, 0, n-1, dp);
+        // System.out.println(res+" Top up");
+        // if(res >0)
+        // {
+        //     // System.out.println(res);
+        //     return true;
+        // }
+        // return false;
+        for(int i=0; i<n-1; i++)
         {
-            // System.out.println(res);
-            return true;
+            for(int j=1; j<n; j++)
+            {
+                dp[i][j] = Math.max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1]);
+            }
         }
-        return false;
+        // for(int i=0; i<n; i++)
+        // {
+        //     for(int j=0; j<n; j++)
+        //     {
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        System.out.println(dp[0][n-1] + " Bottom up");
+        return true;
     }
 }

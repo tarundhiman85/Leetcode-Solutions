f3535387fe998a4cf1e155e45c66ef65eade245f
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2023-08-13 22:47:21 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (79.72%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int f(int[] pile, int i, int j, int[][] dp)
-    {
-        if(i>j)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=0)
-        {
-            return dp[i][j];
-        }
-        return dp[i][j] = Math.max(pile[i] - f(pile, i+1, j, dp), pile[j] - f(pile, i, j-1, dp));
-    }
+    // public int f(int[] pile, int i, int j, int[][] dp)
+    // {
+    //     if(i>j)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=0)
+    //     {
+    //         return dp[i][j];
+    //     }
+    //     return dp[i][j] = Math.max(pile[i] - f(pile, i+1, j, dp), pile[j] - f(pile, i, j-1, dp));
+    // }
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        int[][] dp = new int[n][n];
+        // int n = piles.length;
+        // int[][] dp = new int[n][n];
         // int res = f(piles, 0, n-1, dp);
         // System.out.println(res+" Top up");
         // if(res >0)
@@ -22,13 +22,13 @@ class Solution {
         //     return true;
         // }
         // return false;
-        for(int i=0; i<n-1; i++)
-        {
-            for(int j=1; j<n; j++)
-            {
-                dp[i][j] = Math.max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1]);
-            }
-        }
+        // for(int i=0; i<n-1; i++)
+        // {
+        //     for(int j=1; j<n; j++)
+        //     {
+        //         dp[i][j] = Math.max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1]);
+        //     }
+        // }
         // for(int i=0; i<n; i++)
         // {
         //     for(int j=0; j<n; j++)
@@ -37,7 +37,7 @@ class Solution {
         //     }
         //     System.out.println();
         // }
-        System.out.println(dp[0][n-1] + " Bottom up");
+        // System.out.println(dp[0][n-1] + " Bottom up");
         return true;
     }
 }

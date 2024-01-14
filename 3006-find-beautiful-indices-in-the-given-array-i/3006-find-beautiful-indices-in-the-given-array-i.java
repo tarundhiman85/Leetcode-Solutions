79bf7a48490d159e8142f5d48d13eafd95d5c37f
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2024-01-14 10:53:59 +0530

    Time: 24 ms (33.33%), Space: 47.6 MB (33.33%) - LeetHub

---
+++ 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> beautifulIndices(String s, String a, String b, int k) {
+        List<Integer> result = new ArrayList<>();
+
+        List<Integer> indicesA = new ArrayList<>();
+        List<Integer> indicesB = new ArrayList<>();
+
+        for (int i = 0; i <= s.length() - a.length(); i++) {
+            if (s.substring(i, i + a.length()).equals(a)) {
+                indicesA.add(i);
+            }
+        }
+            
+        for (int i = 0; i <= s.length() - b.length(); i++) {
+            if (s.substring(i, i + b.length()).equals(b)) {
+                indicesB.add(i);
+            }
+        }
+
+        for (int i : indicesA) {
+            int low = 0, high = indicesB.size() - 1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                int j = indicesB.get(mid);
+
+                if (Math.abs(i - j) <= k) {
+                    result.add(i);
+                    break;
+                } else if (j < i) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return result;
+    }
+}

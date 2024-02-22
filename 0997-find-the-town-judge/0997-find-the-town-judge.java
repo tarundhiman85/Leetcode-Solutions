d826b8fba3ebc8b1afcc8f28eb5ae15baef96e47
Tarun <53346586+tarundhiman85@users.noreply.github.com>
2024-02-22 21:09:20 +0530

    Time: 3 ms (71.04%), Space: 53.5 MB (67.26%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,16 +1,20 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-         int arr[] = new int[n + 1]; // Define an array of length n+1
-        
-        for(int i = 0; i < trust.length; i++){ // loop through the 'trust' array
-            arr[trust[i][0]]--; // if a given person trust someone then decrement the count of his index
-            arr[trust[i][1]]++; // increment the count of the index whom he trusts
-            
+        int[] trusting = new int[n + 1];
+        int[] trusted = new int[n + 1];
+
+        for (int i = 0; i < trust.length; i++) {
+            trusting[trust[i][0]]++;
+            trusted[trust[i][1]]++;
         }
-        // In last, check the 'arr' array that if there is any index having count equals to n-1
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] == n - 1) return i; // person doesnt trust anybody but everyone else trust him. Return their index
+
+        int ans = -1;
+
+        for (int i = 1; i <= n; i++) {
+            if (trusting[i] == 0 && trusted[i] == n - 1)
+                ans = i;
         }
-        return -1;
+
+        return ans;
     }
 }
